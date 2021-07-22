28a40368346f709ba08e932f2e4655937fe408b0
anshuman-sidtm <anshumansen.sitm@gmail.com>
2021-07-22 09:13:38 +0530

    Fresh Project

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,4 @@
 mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --user
-python -m pip install Flask
-#sdssdsds
+python3.7 get-pip.py --user
+python3.7 -m pip install Flask
